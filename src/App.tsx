4a48484c79e5a2e58449a5c57ee0bbee15921bd8
김영재 <gotjr44@naver.com>
2025-11-12 22:12:33 +0900

    feat : app.tsx 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+{
+  /*
+  주석은 중괄호 안에
+  */
+}
 function App() {
+  let name = "리액트";
+  const style = {
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "48px",
+    fontWeight: "bold",
+    padding: "20px",
+  };
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+      <h1
+        style={{
+          backgroundColor: "purple",
+          color: "white",
+          fontSize: "48px",
+          fontWeight: "bold",
+          padding: "20px",
+        }}
+      >
+        Hello,
+        {name === "리액트" ? <h1>YES</h1> : null}
+      </h1>
+      <p>반갑습니다.</p>
     </div>
   );
 }
